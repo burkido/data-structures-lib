5906b763b3b4c3ad2d487f5da8079307d5230465
Burak Karaduman <karadumanburak000@gmail.com>
2022-09-25 13:29:43 +0300

    queue and stack commands were added for compiling and linking the files

--- ds/linear/compile.bat
+++ ds/linear/compile.bat
@@ -1,6 +1,12 @@
 gcc -c linked_list.c -o linked_list.o
 ar rcs liblinked_list.a linked_list.o
 
+gcc -c stack.c -o stack.o
+ar rcs libstack.a stack.o
+
+gcc -c queue.c -o queue.o
+ar rcs libqueue.a queue.o
+
 gcc -c helper.c -o helper.o
 ar rcs libhelper.a helper.o
 
